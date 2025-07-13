465de28dd547407ab2483f38b57fa35b80eaef49
aloysathekge <sathekgealoy@gmail.com>
2025-07-13 18:53:47 +0200

    register spotify tools for testing

--- pori/tools_box/__init__.py
+++ pori/tools_box/__init__.py
@@ -1,9 +1,11 @@
 from .math_tools import register_math_tools
 from .core_tools import register_core_tools
 from .number_tools import register_number_tool
+from .spotify_tools import register_spotify_tools
 
 
 def register_all_tools(registry):
     register_math_tools(registry)
     register_core_tools(registry)
     register_number_tool(registry)
+    register_spotify_tools(registry)
